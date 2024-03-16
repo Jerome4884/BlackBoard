8470a8e1bd22b01c73740fd91e525710d482e91c
Jerome <118047554+Jerome4884@users.noreply.github.com>
2024-03-16 22:05:51 +0100

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://Jerome:nEWdIHqHXCb1Gs21@cluster0.top9reg.mongodb.net/blackboard';
+const connectionString = 'mongodb+srv://@cluster.top9reg.mongodb.net/blackboard';
 
 mongoose.connect(connectionString, {connectTimeoutMS: 2000})
     .then(() => console.log('Database is connected'))
